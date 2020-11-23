c122124af36d659a89febb982fcee20b636e9b0f
Diane Crame <dcrame@icloud.com>
2020-11-22 18:27:24 -0800

    initialized state of null for lat

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
