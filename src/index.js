896d85146d88f67b3ac7524a5babad7c7047bbc8
Diane Crame <dcrame@icloud.com>
2020-11-22 17:06:19 -0800

    added geolocation function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
   return <div>Hi there!</div>;
 };
 
