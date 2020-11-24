9a5574f8a98ebac347b77249fedcd83f054324c9
Diane Crame <dcrame@icloud.com>
2020-11-23 18:31:06 -0800

    refactored state

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  //   constructor(props) {
+  //     super(props);
 
-    this.state = { lat: null, errorMessage: "" };
-  }
+  //     this.state = { lat: null, errorMessage: "" };
+  //   }
+
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
