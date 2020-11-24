e80bc32ff42e41fc39ababaa44af36d31c81ff37
Diane Crame <dcrame@icloud.com>
2020-11-23 19:34:16 -0800

    styled text and included corresponding icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,7 +10,17 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  return <div>Latitude: {props.lat}</div>;
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Let's hit the beach!";
+  const icon = season === "winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
