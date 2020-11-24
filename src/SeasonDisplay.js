06f8e519af5320ae2892f23063594f52e98e3cdf
Diane Crame <dcrame@icloud.com>
2020-11-23 19:22:56 -0800

    ternary conditional to determine current season based on geolocation

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = (props) => {
-  return <div>{props.lat}</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  return <div>Latitude: {props.lat}</div>;
 };
 
 export default SeasonDisplay;
