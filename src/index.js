35d4a0248cfe7557ab31788e2230e7dfea87bb58
Diane Crame <dcrame@icloud.com>
2020-11-23 20:00:06 -0800

    displays loading wheel during initialize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   //   constructor(props) {
@@ -27,7 +28,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
