c90e4d3bcbf2adb444c44e302202d656be3dc2fb
Diane Crame <dcrame@icloud.com>
2020-11-24 15:09:32 -0800

    created helper function to refactor App render

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage} </div>;
     }
@@ -29,6 +29,10 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
